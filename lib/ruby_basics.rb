f54a0cdbfe7ccd9a02e005b1a78e8ec78a83f9df
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-14 11:20:02 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,8 +6,8 @@ def assign_variable(value)
   value = "Bob"
 end
 
-def argue(phrase = "I'm right and you are wrong")
-  
+def argue(phrase = "I'm right and you are wrong!")
+  phrase
 end
 
 def greeting(greeting = "Hi there", name = "Bobby!")
