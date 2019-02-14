ce16a80c2841810c69834906045f7aa8b0c2a6ff
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-14 11:16:49 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,18 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
 def argue
   
 end
 
-def greeting
-
+def greeting(greeting = "Hi there", name = "Bobby!")
+  greeting
+  name
 end
 
 def return_a_value
